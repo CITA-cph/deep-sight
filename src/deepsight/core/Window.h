04d64c62eec32d4503a6557ad53b578b3a8b3bc0
Tom Svilans <tom.svilans@gmail.com>
2020-09-25 11:25:29 +0200

    feat(window): added prelim window class

---
+++ src/deepsight/core/Window.h
@@ -0,0 +1,18 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <glad/glad.h>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include "linmath.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+namespace RawLam
+{
+	int TestOpenGLWindow();
+}
+
+#endif
