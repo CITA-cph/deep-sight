65ee3c6ef417c966511c3387e9dd9b6fa9df969a
Tom Svilans <email@example.com>
2023-04-25 18:49:41 +0200

    feat(log): added log models and knot region generation

---
+++ src/RawLamb/Geometry.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rhino.Geometry;
+
+namespace RawLamb
+{
+    public static class Geometry
+    {
+        /// <summary>
+        /// Determines whether or not a point is inside a cone.
+        /// </summary>
+        /// <param name="apex">Apex point of the cone.</param>
+        /// <param name="axis">Normalized axis of the cone.</param>
+        /// <param name="height">Height of the cone.</param>
+        /// <param name="radius">Radius of the cone.</param>
+        /// <param name="pt">Point to check.</param>
+        /// <returns>1 if the point is inside the cone, 0 if it is on the cone, and -1 if it is outside the cone.</returns>
+        public static int PointInConeSquared(Point3d apex, Vector3d axis, double height, double radius, Point3d pt)
+        {
+            var A = pt - apex;      // Vector from sample point to apex
+            var dot = A * axis;     // Length of axis at closest point
+            var t = dot / height;   // Proportion of length at closest point
+            var r = radius * t;     // Radius of cone at closest point
+            var B = pt - (apex + axis * dot); // Vector from sample point to closest point
+
+            // Compare length of vector B to the radius at closest point
+            return Math.Sign(r * r - (B.X * B.X + B.Y * B.Y + B.Z * B.Z));
+        }
+
+        /// <summary>
+        /// Creates a truncated conde.
+        /// </summary>
+        /// <param name="plane">Base plane of cone.</param>
+        /// <param name="height">Height of cone.</param>
+        /// <param name="radius1">Bottom radius of cone.</param>
+        /// <param name="radius2">Top radius of cone.</param>
+        /// <param name="cap_bottom">True to cap the bottom of the cone.</param>
+        /// <param name="cap_top">True to cap the top of the cone.</param>
+        /// <returns></returns>
+        public static Brep CreateTruncatedCone(Plane plane, double height, double radius1, double radius2, bool cap_bottom = false, bool cap_top = false)
+        {
+            var plane2 = new Plane(plane.Origin + plane.ZAxis * height, plane.ZAxis);
+            Circle bottom_circle = new Circle(plane, radius1);
+            Circle top_circle = new Circle(plane2, radius2);
+
+            LineCurve shapeCurve = new LineCurve(bottom_circle.PointAt(0), top_circle.PointAt(0));
+            Line axis = new Line(bottom_circle.Center, top_circle.Center);
+            RevSurface revsrf = RevSurface.Create(shapeCurve, axis);
+            return Brep.CreateFromRevSurface(revsrf, cap_bottom, cap_top);
+        }
+    }
+}
--- src/RawLamb/Knot.cs
+++ src/RawLamb/Knot.cs
@@ -25,5 +25,67 @@ namespace RawLamb
             Index = index;
             DeadKnotRadius = dead_knot_radius;
         }
+
+        /// <summary>
+        /// Create Fibre Deviation Region (FDR) or TR (Transition Region) knot regions as per 
+        /// Lukacevic, M., Kandler, G., Hu, M., Olsson, 
+        /// A., & Füssl, J. (2019). A 3D model for knots and related fiber deviations in sawn 
+        /// timber for prediction of mechanical properties of boards. Materials and Design, 166. 
+        /// https://doi.org/https://doi.org/10.1016/j.matdes.2019.107617.
+        /// Depending on the knot length and radius, this will either create a simple region
+        /// (single cone) or a complex region (compound cone) consisting of a simple cone and a
+        /// truncated cone.
+        /// </summary>
+        /// <param name="axis">Knot axis. The axis length corresponds to the knot length.</param>
+        /// <param name="radius">Knot radius.</param>
+        /// <param name="factor">Multiplication factor of apex angle (6 for FDR, 7 for TR).</param>
+        /// <param name="radius_limit">Maximum thickness of region (25 for FDR, 30 for TR).</param>
+        /// <param name="radius1">Radius of cone if simple; first radius of compound cone if not.</param>
+        /// <param name="radius2">0 if simple; second radius of compound cone if not.</param>
+        /// <param name="t">Proportion of the knot length at which the first radius occurs. If 
+        /// it is a simple region, this will be 1.0.</param>
+        /// <returns>Region geometry as array of Breps.</returns>
+        public static Brep[] CreateRegion(Line axis, double radius, double factor, double radius_limit,
+            out double radius1, out double radius2, out double t)
+        {
+            double length = axis.Length;
+            double slope = radius / length;
+
+            double angle = Math.Atan(slope);
+            double slope_start = Math.Tan(angle * factor);
+
+            /*
+            Solve the pair of linear equations (y = mx + b) where:
+              y = slope * x + radius_limit -> the offset cone with the same slope
+              y = slope_start * x -> the wide cone with the same apex
+            Solve for x...
+            */
+            double x = radius_limit / (slope_start - slope);
+            radius1 = radius + radius_limit;
+
+            var plane0 = new Plane(axis.From, axis.Direction);
+
+            // If the intersection of the two lines is within the range of
+            // the knot axis...
+            if (length > x)
+            {
+                radius2 = x * slope_start;
+                var plane1 = new Plane(plane0.Origin + plane0.ZAxis * x, plane0.ZAxis);
+                t = x / length;
+                return new Brep[]{
+                    new Cone(plane0, x, radius2).ToBrep(false),
+                    Geometry.CreateTruncatedCone(plane1, length - x, radius2, radius1, false, true)
+                };
+            }
+            // Otherwise, the intersection is past the end of the knot and
+            // doesn't matter...
+            else
+            {
+                t = 1.0;
+                radius2 = 0.0;
+                return new Brep[] { new Cone(plane0, length, radius1).ToBrep(true) };
+            }
+        }
+
     }
 }
